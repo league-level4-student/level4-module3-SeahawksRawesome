50d6b70d237e2ad45b29353abe2243780b727f6a
League <league@iMac.attlocal.net>
2019-10-13 17:22:33 -0700

    Bruh module 3
    second package

---
+++ src/_01_introduction/CustomException.java
@@ -0,0 +1,10 @@
+package _01_introduction;
+
+public class CustomException extends Exception {
+	public static void main(String [] args) {
+		//terminate();
+	}
+		public static void terminate() {
+			System.exit(0);
+		}
+}
